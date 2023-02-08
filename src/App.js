5a85882bae37e966dadd4bd6a82a8015eff8c1e1
Lana-1167 <lana_lana67@ukr.net>
2023-02-08 20:27:11 +0100

    created list of shows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
 import './App.css';
 import showsData from  './shows-data'
+
+function Show(props) {
+  return (
+    <div className="Show">
+      <img className="Show-image" src={ props.showObject.image.medium} 
+      alt={ props.showObject.name}/>
+    <h2>{props.showObject.name}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>The Best TV Shows App</h1>
+      <h1>The Best TV Show App</h1>
+      {
+        showsData.map(item=><Show showObject={item}></Show> )
+      }
+      
+      
+     
+      
     </div>
   );
 }
+
+
+
+
 
 export default App;
