346bfa3abfa40383e1fbf3d62715bbdd19376655
Lana-1167 <lana_lana67@ukr.net>
2023-02-22 20:23:22 +0100

    split prodject in component

---
+++ src/componets/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <div className="Header">
+            <div className="Logo"></div>
+            <div className="Navigation"></div>
+            <a href="/">Home</a>
+            <a href="/favorites">Favorites</a>
+            <a href="upcoming">Upcoming</a>
+
+
+        </div>
+    )
+}
+
+export default Header
