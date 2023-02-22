1685db1c9b711e5416e454fc9466e139b2b6b9c4
Lana-1167 <lana_lana67@ukr.net>
2023-02-22 20:29:04 +0100

    split app in multiple components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
 import './App.css';
-import showsData from  './shows-data'
+import showsData from  './shows-data';
 
-function Show(props) {
-  return (
-    <div className="Show">
-      <img className="Show-image" src={ props.showObject.image.medium} 
-      alt={ props.showObject.name}/>
-    <h2>{props.showObject.name}</h2>
-    </div>
-  )
-}
-
+import Show from ´./components/Show´;
+import ShowList from ´./componets/ShowList´;
+import Header from ´./component/Header´;
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>The Best TV Show App</h1>
-      {
-        showsData.map(x=><Show showObject={x}></Show> )
-      }
-      
+      <ShowList shows={showsData}/>
       
      
       
