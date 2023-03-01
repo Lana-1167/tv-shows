3daa9f8a69287f7d866b2bde6935e0789a2fd3c7
Lana-1167 <lana_lana67@ukr.net>
2023-03-01 20:12:24 +0100

    installed router

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      
       <h1>The Best TV Show App</h1>
       <ShowList shows={showsData}/>
       
---
+++ src/pages/Favorites.js
@@ -0,0 +1,6 @@
+function Favorites() {
+    return (
+        <h2>This is the Favorites page</h2>
+    )
+}
+export default Favorites
